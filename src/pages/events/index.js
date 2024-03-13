8742a6c691fd0fb1c4fc14e75635ef68d521572f
lucas kristandt <lkristandt@live.de>
2024-03-13 23:55:17 +0100

    make event clickable

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { useTheme } from '@mui/material/styles';
-import { Grid, Typography, CircularProgress, Stack, List, ListItem, ListItemText, ListItemIcon } from '@mui/material';
+import { DialogContent, DialogContentText, Dialog, DialogTitle, DialogActions, Button } from '@mui/material';
 import ColoredSection from 'components/pageLayout/header/ColoredSection';
 import LayoutBox from 'components/LayoutBox/index';
 
@@ -11,6 +11,7 @@ import deLocale from '@fullcalendar/core/locales/de';
 import ical from './calendar.ics';
 
 const Events = () => {
+  const [dialogSettings, setDialogSettings] = useState(null);
   const theme = useTheme();
   const headerBgColor = `radial-gradient(circle at 2% 10%, ${theme.palette.primary.dark}, transparent 100%),radial-gradient(circle at 95% 20%, ${theme.palette.primary.main}, transparent 100%),radial-gradient(circle at 25% 90%, ${theme.palette.primary.light}, transparent 100%)`;
 
@@ -32,6 +33,7 @@ const Events = () => {
         }}
       >
         <FullCalendar
+          key={'test'}
           plugins={[dayGridPlugin, iCalendarPlugin]}
           initialView="dayGridMonth"
           locales={[deLocale]}
@@ -41,15 +43,27 @@ const Events = () => {
             format: 'ics'
           }}
           eventClick={function (info) {
-            alert('Event: ' + info.event.title);
-            alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-            alert('View: ' + info.view.type);
-
-            // change the border color just for fun
-            info.el.style.borderColor = 'red';
+            setDialogSettings({
+              title: info.event.title,
+              description: info.event.extendedProps.description
+            });
           }}
         />
       </LayoutBox>
+      <Dialog
+        open={!!dialogSettings}
+        onClose={() => setDialogSettings(null)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{dialogSettings?.title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{dialogSettings?.description}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setDialogSettings(null)}>Schließen</Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };
