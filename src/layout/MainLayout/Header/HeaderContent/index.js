74f47d72320791a2eeb3b5cf3122344ea1c0c9e4
lucas kristandt <lkristandt@live.de>
2024-05-02 21:01:46 +0200

    ticket adjustments and fixes

--- src/layout/MainLayout/Header/HeaderContent/index.js
+++ src/layout/MainLayout/Header/HeaderContent/index.js
@@ -3,8 +3,9 @@ import Profile from './Profile';
 import { useContext } from 'react';
 import { UserContext } from 'context/user';
 import { useTheme } from '@mui/material/styles';
-import { Stack, Typography } from '../../../../../node_modules/@mui/material/index';
+import { Stack, Typography } from '@mui/material/index';
 import { NavigationContext } from 'context/navigation/index';
+import { useLocation } from 'react-router-dom/dist/index';
 
 // ==============================|| HEADER - CONTENT ||============================== //
 
@@ -12,10 +13,15 @@ const HeaderContent = () => {
   const { user } = useContext(UserContext);
   const { useDrawerNav } = useContext(NavigationContext);
   const theme = useTheme();
+  const location = useLocation();
 
   return (
     <>
-      <Stack flexDirection="column" marginTop={1}>
+      <Stack
+        flexDirection="column"
+        marginTop={1}
+        {...(location.pathname !== '/office/dashboard' ? { sx: { opacity: 0 }, 'aria-hidden': true } : {})}
+      >
         <Typography>Willkommen zurück,</Typography>
         <Typography sx={{ fontSize: 36, fontWeight: theme.typography.fontWeightBold }}>{user?.displayName}</Typography>
       </Stack>
