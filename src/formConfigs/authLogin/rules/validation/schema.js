3612d75714b91fb3ace81786967a8d93246c7092
Lucas Kristandt <lucas.kristandt@pinuts.de>
2024-06-06 09:52:15 +0200

    improve credentials change

--- src/formConfigs/authLogin/rules/validation/schema.js
+++ src/formConfigs/authLogin/rules/validation/schema.js
@@ -1,8 +1,10 @@
-import { object, string } from 'yup';
+import { object, string, ref } from 'yup';
 
 const validationSchema = object().shape({
   email: string().email('Geben Sie eine gültige Email an.').required('Geben Sie eine Email an.'),
-  password: string().required('Geben Sie ein Passwort an.')
+  emailConfirm: string().oneOf([ref('email'), null], 'Die Emails müssen übereinstimmen.'),
+  password: string().required('Geben Sie ein Passwort an.').min(8, 'Das Passwort muss mindestens 8 Zeichen lang sein.'),
+  passwordConfirm: string().oneOf([ref('password'), null], 'Die Passwörter müssen übereinstimmen.')
 });
 
 export default validationSchema;
