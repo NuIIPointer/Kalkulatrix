116d0720088f7c893bcd210a1b675b829bd8a92a
Lucas Kristandt <lkristandt@ive.de>
2025-02-26 20:13:10 +0100

    fix wrong links on login pages

--- src/components/cards/AuthFooter.js
+++ src/components/cards/AuthFooter.js
@@ -1,10 +1,12 @@
 // material-ui
 import { useMediaQuery, Container, Link, Typography, Stack } from '@mui/material';
+import * as CookieConsent from 'vanilla-cookieconsent';
 
 // ==============================|| FOOTER - AUTHENTICATION ||============================== //
 
 const AuthFooter = () => {
   const matchDownSM = useMediaQuery((theme) => theme.breakpoints.down('sm'));
+  const showConsent = () => CookieConsent.showPreferences();
 
   return (
     <Container maxWidth="xl">
@@ -22,14 +24,23 @@ const AuthFooter = () => {
         </Typography>
 
         <Stack direction={matchDownSM ? 'column' : 'row'} spacing={matchDownSM ? 1 : 3} textAlign={matchDownSM ? 'center' : 'inherit'}>
-          <Typography variant="subtitle2" component={Link} href="/" underline="hover">
+          <Typography variant="subtitle2" component={Link} onClick={showConsent} underline="hover" sx={{ cursor: 'pointer' }}>
+            Cookie-Einstellungen
+          </Typography>
+          <Typography variant="subtitle2" component={Link} href="/kontakt" underline="hover">
             Kontakt
           </Typography>
-          <Typography variant="subtitle2" component={Link} href="/" underline="hover">
+          <Typography variant="subtitle2" component={Link} href="/faq" underline="hover">
+            FAQ
+          </Typography>
+          <Typography variant="subtitle2" component={Link} href="/impressum" underline="hover">
+            Impressum
+          </Typography>
+          <Typography variant="subtitle2" component={Link} href="/datenschutz" underline="hover">
             Datenschutzerklärung
           </Typography>
-          <Typography variant="subtitle2" component={Link} href="/" underline="hover">
-            Impressum
+          <Typography variant="subtitle2" component={Link} href="/files/kalkulatrix-allgemeine-geschaeftsbedingungen.pdf" underline="hover">
+            AGB
           </Typography>
         </Stack>
       </Stack>
